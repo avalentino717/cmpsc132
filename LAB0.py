4c811c9a81780ed505965a40ec5c93dab0117253
avalentino717 <agv5187@psu.edu>
2026-01-15 15:44:28 -0500

    first commit

---
+++ LAB0.py
@@ -0,0 +1,26 @@
+def sum_evens(numList):
+    """
+        >>> sum_evens([1,5,-3,5.5,359,8,14,-25,1000])
+        1022.0
+        >>> sum_evens([14,5,-3,5,9.0,8,14,7,-846])
+        -810.0
+        >>> sum_evens([-8.0,-4,1,2,3,4,5,6,12])
+        12.0
+
+        To verify output is being returned, not printed
+        >>> output = sum_evens([1,5,-3,5,45.5,8.5,-5,500,6.7,-25])
+        >>> output
+        500.0
+    """
+    # --- YOU CODE STARTS HERE
+    result = 0
+    for i in numList:
+        if i % 2 == 0:
+            result += i
+    return float(result)
+    pass
+
+if __name__ == "__main__":
+    import doctest
+    ## Uncomment the line below if you want to start testing using the examples in the docstring
+    doctest.testmod() 
---
+++ first.py
@@ -0,0 +1,15 @@
+def square(num):
+    """
+    Docstring for square
+    
+    :param num: Description
+
+    Returns square
+
+        >>> square(10)
+        100
+        >>> square(5)
+        25
+    """
+    final = num * num
+    return final
